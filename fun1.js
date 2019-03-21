7e4bc8cccfd063a036f83b5d5f092d9640bd61ff
Sourabha Bharadwaj B M <36269370+bmsohwinc@users.noreply.github.com>
2019-03-21 16:28:32 +0530

    external file that was called by the cd1.html code

---
+++ fun1.js
@@ -0,0 +1,18 @@
+function say()
+{
+	alert("hello man.... I am from ext file")
+}
+
+var mmmm = "global ji"
+function main()
+{
+	var x,y,z;
+	var mmmm = "local ji"
+	
+	console.log(mmmm)
+	
+}
+
+
+console.log(mmmm)
+main()
